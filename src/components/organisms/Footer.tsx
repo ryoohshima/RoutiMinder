f59e3a3f297930fc0e480fc3ac8d99a3de601b63
ryoohshima <chlslmps@gmail.com>
2023-09-11 02:24:21 +0900

    organismsの作成
    - add: ルーティンリストの作成
    - fix: ヘッダーの修正
    - fix: フッターの修正

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { getBackgroundColor, getBorderColor } from "@/utils/getClass";
 
 const Footer = () => {
+  const bgColor = getBackgroundColor('white');
+  const borderColor = getBorderColor('primary', 'top');
   return (
-    <footer></footer>
+    <footer className={`flex items-center justify-between	 w-full h-12 p-2 fixed bottom-0 left-0 right-0 z-10 ${bgColor} ${borderColor}`}>
+      <Link href={'/'}>
+        <Image src={'/icon/house_line.svg'} alt="" width={24} height={24}/>
+      </Link>
+      <Link href={'/'}>
+        <Image src={'/icon/add_line.svg'} alt="" width={24} height={24}/>
+      </Link>
+      <Link href={'/'}>
+        <Image src={'/icon/me_line.svg'} alt="" width={24} height={24}/>
+      </Link>
+    </footer>
   )
+
 }
 
 export default Footer;
--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { getBackgroundColor } from "@/utils/getClass";
 
 const Header = () => {
+  const bgColor = getBackgroundColor('tertiary');
   return (
-    <header>
+    <header className={`flex items-center w-full h-16 fixed top-0 left-0 right-0 z-10 ${bgColor}`}>
       <Link href={'/'}>
         <Image src={'/logo.png'} alt="" width={200} height={56} />
       </Link>
