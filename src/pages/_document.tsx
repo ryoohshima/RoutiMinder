eec787419a03cd4ba2b77b67075e64f93e66e146
ryoohshima <chlslmps@gmail.com>
2023-09-10 17:39:30 +0900

    headタグの設定

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <title>RoutiMinder</title>
+        <link rel="icon" href="/favicon.png" />
+        <meta name='description'  content='RoutiMinder: Your Day, Your Way. Routinely. Craft and follow your daily routines effortlessly.'/>
+        <meta property='og:title' content='RoutiMinder' />
+        <meta property='og:image' content='/logo_ogp.png'/>
+        <meta property='og:url' content='https://routi-minder.vercel.app/'/>
+        <meta property='og:type' content='website'/>
+      </Head>
       <body>
         <Main />
         <NextScript />
