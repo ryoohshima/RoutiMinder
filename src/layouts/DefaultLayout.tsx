c394e496de4be0141c2035607f1d6ce92dbe7e27
ryoohshima <chlslmps@gmail.com>
2023-09-11 02:24:39 +0900

    レイアウトの修正
    - fix: デフォルトレイアウトの修正

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import Header from '@/components/organisms/Header';
 import Footer from '@/components/organisms/Footer';
+import { getBackgroundColor } from '@/utils/getClass';
 
 const DefaultLayout = ({ children }) => {
+  const bgColor = getBackgroundColor('tertiary');
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className={`flex flex-col min-h-screen ${bgColor}`}>
       <Header />
-      <main className="flex-grow container mx-auto p-4">
+      <main className="px-2 py-20">
         {children}
       </main>
       <Footer />
-
     </div>
   );
 }
